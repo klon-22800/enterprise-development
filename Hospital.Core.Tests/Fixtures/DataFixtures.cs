e972a5769ce168772de8ef5e820e59078fa6a66e
Klon <www.vfdrey@gmail.com>
2025-09-23 19:50:16 +0400

    test data loader

---
+++ Hospital.Core.Tests/Fixtures/DataFixtures.cs
@@ -0,0 +1,47 @@
+﻿using Hospital.Core.Domain.Models;
+using Hospital.Core.Domain.TestData;
+
+namespace Hospital.Core.Tests.Fixtures;
+
+/// <summary>
+/// Fixture for Unit tests
+/// </summary>
+public class TestDataFixture : IDisposable
+{
+    /// <summary>
+    /// Test list of Patients
+    /// </summary>
+    public List<Patient> Patients { get; }
+    /// <summary>
+    /// Test list of Doctors
+    /// </summary>
+    public List<Doctor> Doctors { get; }
+    /// <summary>
+    /// Test list of Specializations
+    /// </summary>
+    public List<Specialization> Specializations { get; }
+    /// <summary>
+    /// Test list of Appointments
+    /// </summary>
+    public List<Appointment> Appointments { get; }
+
+    /// <summary>
+    /// Initializes test data
+    /// </summary>
+    public TestDataFixture()
+    {
+        var (patients, doctors, specializations, appointments) = DataGenerator.GenerateData();
+
+        Patients = patients.ToList();
+        Doctors = doctors.ToList();
+        Specializations = specializations.ToList();
+        Appointments = appointments.ToList();
+    }
+
+    /// <summary>
+    /// Resource disposing
+    /// </summary>
+    public void Dispose()
+    {
+    }
+}
--- Hospital.Core.Tests/UnitTest1.cs
+++ Hospital.Core.Tests/UnitTest1.cs
@@ -1,11 +1,28 @@
-namespace Hospital.Core.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+using Hospital.Core.Domain.Models;
+using Hospital.Core.Tests.Fixtures;
+using System.Linq;
 
-        }
+namespace Hospital.Core.Tests;
+
+public class UnitTest1 : IClassFixture<TestDataFixture>
+{
+    private readonly TestDataFixture _fixture;
+
+    public UnitTest1(TestDataFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public void Doctors_With_Experience_10_Or_More_Should_Be_Displayed()
+    {
+        var result = _fixture.Doctors
+            .Where(d => d.Expirience >= 10)
+            .Select(d => $"Доктор: {d.Surname} {d.Name} {d.Patronymic}, Стаж: {d.Expirience} лет, Специализация: {d.Specialization.Name}")
+            .ToList();
+
+        Assert.NotEmpty(result);
+
+        result.ForEach(Console.WriteLine);
     }
 }
