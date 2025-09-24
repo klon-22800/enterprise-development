540a7bdd5f1aa7b3138711fe0dd03af26c4cebc8
Klon <www.vfdrey@gmail.com>
2025-09-24 23:01:57 +0400

    test done

--- Hospital.Core.Domain/TestData/DataGenerator.cs
+++ Hospital.Core.Domain/TestData/DataGenerator.cs
@@ -10,10 +10,10 @@ public class DataGenerator
         {
             new Specialization { Id = 1, Name = "Хирург" },
             new Specialization { Id = 2, Name = "Терапевт" },
-            new Specialization { Id = 3, Name = "Кардиолог" },
+            new Specialization { Id = 3, Name = "Офтальмолог" },
             new Specialization { Id = 4, Name = "Невролог" },
             new Specialization { Id = 5, Name = "Педиатр" },
-            new Specialization { Id = 6, Name = "Офтальмолог" },
+            new Specialization { Id = 6, Name = "Кардиолог" },
             new Specialization { Id = 7, Name = "Дерматолог" },
             new Specialization { Id = 8, Name = "Ортопед" },
             new Specialization { Id = 9, Name = "Эндокринолог" },
@@ -22,16 +22,16 @@ public class DataGenerator
 
         var doctors = new[]
         {
-            new Doctor { PassportNumber = "1111 000001", Name = "Иван", Surname = "Иванов", Patronymic = "Иванович", BirthDate = new DateOnly(1975, 5, 15), Specialization = specializations[0], Expirience = 20 },
-            new Doctor { PassportNumber = "1111 000002", Name = "Петр", Surname = "Петров", Patronymic = "Петрович", BirthDate = new DateOnly(1980, 3, 21), Specialization = specializations[1], Expirience = 15 },
-            new Doctor { PassportNumber = "1111 000003", Name = "Анна", Surname = "Смирнова", Patronymic = "Сергеевна", BirthDate = new DateOnly(1988, 7, 9), Specialization = specializations[2], Expirience = 10 },
-            new Doctor { PassportNumber = "1111 000004", Name = "Сергей", Surname = "Сидоров", Patronymic = "Андреевич", BirthDate = new DateOnly(1970, 11, 3), Specialization = specializations[3], Expirience = 25 },
-            new Doctor { PassportNumber = "1111 000005", Name = "Мария", Surname = "Кузнецова", Patronymic = "Олеговна", BirthDate = new DateOnly(1990, 1, 17), Specialization = specializations[4], Expirience = 7 },
-            new Doctor { PassportNumber = "1111 000006", Name = "Дмитрий", Surname = "Орлов", Patronymic = "Михайлович", BirthDate = new DateOnly(1983, 6, 30), Specialization = specializations[5], Expirience = 12 },
-            new Doctor { PassportNumber = "1111 000007", Name = "Ольга", Surname = "Морозова", Patronymic = "Александровна", BirthDate = new DateOnly(1985, 4, 12), Specialization = specializations[6], Expirience = 9 },
-            new Doctor { PassportNumber = "1111 000008", Name = "Алексей", Surname = "Федоров", Patronymic = "Игоревич", BirthDate = new DateOnly(1978, 9, 23), Specialization = specializations[7], Expirience = 18 },
-            new Doctor { PassportNumber = "1111 000009", Name = "Елена", Surname = "Соболева", Patronymic = "Степановна", BirthDate = new DateOnly(1992, 2, 2), Specialization = specializations[8], Expirience = 5 },
-            new Doctor { PassportNumber = "1111 000010", Name = "Виктор", Surname = "Егоров", Patronymic = "Владимирович", BirthDate = new DateOnly(1969, 12, 25), Specialization = specializations[9], Expirience = 30 },
+            new Doctor { PassportNumber = "1111 100001", Name = "Иван", Surname = "Иванов", Patronymic = "Иванович", BirthDate = new DateOnly(1975, 5, 15), Specialization = specializations[0], Expirience = 20 },
+            new Doctor { PassportNumber = "2222 200002", Name = "Петр", Surname = "Петров", Patronymic = "Петрович", BirthDate = new DateOnly(1980, 3, 21), Specialization = specializations[1], Expirience = 15 },
+            new Doctor { PassportNumber = "3333 300003", Name = "Анна", Surname = "Смирнова", Patronymic = "Сергеевна", BirthDate = new DateOnly(1988, 7, 9), Specialization = specializations[2], Expirience = 10 },
+            new Doctor { PassportNumber = "4444 400004", Name = "Сергей", Surname = "Сидоров", Patronymic = "Андреевич", BirthDate = new DateOnly(1970, 11, 3), Specialization = specializations[3], Expirience = 25 },
+            new Doctor { PassportNumber = "5555 500005", Name = "Мария", Surname = "Кузнецова", Patronymic = "Олеговна", BirthDate = new DateOnly(1990, 1, 17), Specialization = specializations[4], Expirience = 7 },
+            new Doctor { PassportNumber = "6666 600006", Name = "Дмитрий", Surname = "Орлов", Patronymic = "Михайлович", BirthDate = new DateOnly(1983, 6, 30), Specialization = specializations[5], Expirience = 12 },
+            new Doctor { PassportNumber = "7777 700007", Name = "Ольга", Surname = "Морозова", Patronymic = "Александровна", BirthDate = new DateOnly(1985, 4, 12), Specialization = specializations[6], Expirience = 9 },
+            new Doctor { PassportNumber = "8888 800008", Name = "Алексей", Surname = "Федоров", Patronymic = "Игоревич", BirthDate = new DateOnly(1978, 9, 23), Specialization = specializations[7], Expirience = 18 },
+            new Doctor { PassportNumber = "9999 900009", Name = "Елена", Surname = "Соболева", Patronymic = "Степановна", BirthDate = new DateOnly(1992, 2, 2), Specialization = specializations[8], Expirience = 5 },
+            new Doctor { PassportNumber = "0000 100010", Name = "Виктор", Surname = "Егоров", Patronymic = "Владимирович", BirthDate = new DateOnly(1969, 12, 25), Specialization = specializations[9], Expirience = 30 },
         };
 
         var patients = new[]
@@ -45,21 +45,22 @@ public class DataGenerator
             new Patient { PassportNumber = "2222 000007", Name = "Игорь", Surname = "Васильев", Patronymic = "Дмитриевич", BirthDate = new DateOnly(1990, 11, 8), Address = "Школьная 22", Gender = "М", BloodType = BloodType.A, RhesusFactor = RhesusFactor.Positive, PhoneNumber = "+79007777777" },
             new Patient { PassportNumber = "2222 000008", Name = "Алина", Surname = "Григорьева", Patronymic = "Станиславовна", BirthDate = new DateOnly(1998, 1, 30), Address = "Молодежная 3", Gender = "Ж", BloodType = BloodType.O, RhesusFactor = RhesusFactor.Negative, PhoneNumber = "+79008888888" },
             new Patient { PassportNumber = "2222 000009", Name = "Руслан", Surname = "Александров", Patronymic = "Владимирович", BirthDate = new DateOnly(1982, 7, 19), Address = "Заречная 12", Gender = "М", BloodType = BloodType.AB, RhesusFactor = RhesusFactor.Negative, PhoneNumber = "+79009999999" },
-            new Patient { PassportNumber = "2222 000010", Name = "Юлия", Surname = "Тарасова", Patronymic = "Егоровна", BirthDate = new DateOnly(1996, 9, 25), Address = "Пушкина 5", Gender = "Ж", BloodType = BloodType.B, RhesusFactor = RhesusFactor.Positive, PhoneNumber = "+79000000000" },
+            new Patient { PassportNumber = "2222 000010", Name = "Юлия", Surname = "Тарасова", Patronymic = "Егоровна", BirthDate = new DateOnly(1992, 9, 25), Address = "Пушкина 5", Gender = "Ж", BloodType = BloodType.B, RhesusFactor = RhesusFactor.Positive, PhoneNumber = "+79000000000" },
         };
 
         var appointments = new[]
         {
-            new Appointment { AppointmentTime = new DateTime(2025, 10, 1, 9, 0, 0), CabinetNumber = "101", IsRepeated = false, Patient = patients[0], Doctor = doctors[0] },
-            new Appointment { AppointmentTime = new DateTime(2025, 10, 1, 10, 0, 0), CabinetNumber = "102", IsRepeated = true, Patient = patients[1], Doctor = doctors[1] },
-            new Appointment { AppointmentTime = new DateTime(2025, 10, 1, 11, 0, 0), CabinetNumber = "103", IsRepeated = false, Patient = patients[2], Doctor = doctors[2] },
-            new Appointment { AppointmentTime = new DateTime(2025, 10, 1, 12, 0, 0), CabinetNumber = "104", IsRepeated = false, Patient = patients[3], Doctor = doctors[3] },
-            new Appointment { AppointmentTime = new DateTime(2025, 10, 1, 13, 0, 0), CabinetNumber = "105", IsRepeated = true, Patient = patients[4], Doctor = doctors[4] },
-            new Appointment { AppointmentTime = new DateTime(2025, 10, 1, 14, 0, 0), CabinetNumber = "106", IsRepeated = false, Patient = patients[5], Doctor = doctors[5] },
-            new Appointment { AppointmentTime = new DateTime(2025, 10, 1, 15, 0, 0), CabinetNumber = "107", IsRepeated = true, Patient = patients[6], Doctor = doctors[6] },
-            new Appointment { AppointmentTime = new DateTime(2025, 10, 1, 16, 0, 0), CabinetNumber = "108", IsRepeated = false, Patient = patients[7], Doctor = doctors[7] },
-            new Appointment { AppointmentTime = new DateTime(2025, 10, 1, 17, 0, 0), CabinetNumber = "109", IsRepeated = true, Patient = patients[8], Doctor = doctors[8] },
-            new Appointment { AppointmentTime = new DateTime(2025, 10, 1, 18, 0, 0), CabinetNumber = "110", IsRepeated = false, Patient = patients[9], Doctor = doctors[9] },
+            new Appointment { AppointmentTime = new DateTime(2025, 1, 1, 9, 0, 0), CabinetNumber = "101", IsRepeated = false, Patient = patients[0], Doctor = doctors[0] },
+            new Appointment { AppointmentTime = new DateTime(2025, 3, 1, 10, 0, 0), CabinetNumber = "102", IsRepeated = true, Patient = patients[1], Doctor = doctors[1] },
+            new Appointment { AppointmentTime = new DateTime(2025, 5, 1, 11, 0, 0), CabinetNumber = "103", IsRepeated = false, Patient = patients[2], Doctor = doctors[2] },
+            new Appointment { AppointmentTime = new DateTime(2025, 7, 1, 12, 0, 0), CabinetNumber = "104", IsRepeated = false, Patient = patients[3], Doctor = doctors[3] },
+            new Appointment { AppointmentTime = new DateTime(2025, 9, 1, 13, 0, 0), CabinetNumber = "105", IsRepeated = true, Patient = patients[4], Doctor = doctors[4] },
+            new Appointment { AppointmentTime = new DateTime(2025, 11, 1, 14, 0, 0), CabinetNumber = "106", IsRepeated = false, Patient = patients[5], Doctor = doctors[5] },
+            new Appointment { AppointmentTime = new DateTime(2025, 2, 1, 15, 0, 0), CabinetNumber = "107", IsRepeated = true, Patient = patients[6], Doctor = doctors[6] },
+            new Appointment { AppointmentTime = new DateTime(2025, 4, 1, 16, 0, 0), CabinetNumber = "108", IsRepeated = false, Patient = patients[7], Doctor = doctors[9] },
+            new Appointment { AppointmentTime = new DateTime(2025, 6, 1, 17, 0, 0), CabinetNumber = "109", IsRepeated = true, Patient = patients[8], Doctor = doctors[9] },
+            new Appointment { AppointmentTime = new DateTime(2025, 8, 1, 18, 0, 0), CabinetNumber = "110", IsRepeated = false, Patient = patients[9], Doctor = doctors[9] },
+            new Appointment { AppointmentTime = new DateTime(2025, 2, 1, 10, 0, 0), CabinetNumber = "101", IsRepeated = false, Patient = patients[9], Doctor = doctors[0] },
         };
 
         return (patients, doctors, specializations, appointments);
--- Hospital.Core.Tests/UnitTest1.cs
+++ Hospital.Core.Tests/UnitTest1.cs
@@ -14,15 +14,129 @@ public class UnitTest1 : IClassFixture<TestDataFixture>
     }
 
     [Fact]
-    public void Doctors_With_Experience_10_Or_More_Should_Be_Displayed()
+    public void DoctorsWithExperienceMoreThan10()
     {
-        var result = _fixture.Doctors
+        var doctors = _fixture.Doctors;
+
+        var passportNumbers = doctors
             .Where(d => d.Expirience >= 10)
-            .Select(d => $"Доктор: {d.Surname} {d.Name} {d.Patronymic}, Стаж: {d.Expirience} лет, Специализация: {d.Specialization.Name}")
-            .ToList();
+            .OrderBy(d => d.PassportNumber)
+            .Select(d => d.PassportNumber)
+            .ToArray();
 
-        Assert.NotEmpty(result);
+        var expected = new[]
+        {
+            "0000 100010", 
+            "1111 100001", 
+            "2222 200002", 
+            "3333 300003", 
+            "4444 400004",
+            "6666 600006",
+            "8888 800008" 
+        };
 
-        result.ForEach(Console.WriteLine);
+        Assert.Equal(expected, passportNumbers);
     }
+
+    [Fact]
+    public void PatientsOfSpecificDoctor()
+    {
+        var doctorPassport = "0000 100010"; 
+        var appointments = _fixture.Appointments;
+
+        var patientPassports = appointments
+            .Where(a => a.Doctor.PassportNumber == doctorPassport)
+            .Select(a => a.Patient)
+            .OrderBy(p => p.Surname)
+            .ThenBy(p => p.Name)
+            .ThenBy(p => p.Patronymic)
+            .Select(p => p.PassportNumber)
+            .ToArray();
+
+        var expected = new[]
+        {
+            "2222 000009", "2222 000008", "2222 000010"
+        };
+
+        Assert.Equal(expected, patientPassports);
+    }
+
+    [Fact]
+    public void CountOfRepeatedAppointmentsPerPatientLastMonth()
+    {
+        var appointments = _fixture.Appointments;
+        var today = new DateTime(2025, 9, 24); 
+        var monthAgo = today.AddMonths(-1);
+
+        var repeatedCounts = appointments
+            .Where(a => a.IsRepeated && a.AppointmentTime >= monthAgo && a.AppointmentTime <= today)
+            .GroupBy(a => a.Patient.PassportNumber)
+            .Select(g => new
+            {
+                PatientPassport = g.Key,
+                Count = g.Count()
+            })
+            .OrderBy(r => r.PatientPassport)
+            .ToArray();
+
+
+        var expected = new[]
+        {
+            new { PatientPassport = "2222 000005", Count = 1 },
+        };
+
+
+        Assert.Equal(expected, repeatedCounts);
+    }
+
+    [Fact]
+    public void PatientsOver30WithAppointmentsToMultipleDoctors()
+    {
+        var appointments = _fixture.Appointments;
+        var today = new DateOnly(2025, 9, 24); 
+        var ageLimit = today.AddYears(-30);
+
+        var patientPassports = appointments
+            .Where(a => a.Patient.BirthDate <= ageLimit)
+            .GroupBy(a => a.Patient.PassportNumber)
+            .Where(g => g.Select(a => a.Doctor.PassportNumber).Distinct().Count() > 1)
+            .Select(g => g.Key) 
+            .Join(
+                appointments.Select(a => a.Patient).Distinct(),
+                passport => passport,
+                patient => patient.PassportNumber,
+                (passport, patient) => new { patient.PassportNumber, patient.BirthDate }
+            )
+            .OrderBy(p => p.BirthDate)
+            .Select(p => p.PassportNumber)
+            .ToArray();
+
+        var expected = new string[] {"2222 000010"};
+
+        Assert.Equal(expected, patientPassports);
+    }
+
+    [Fact]
+    public void AppointmentsCurrentMonthInSpecificCabinet()
+    {
+        var appointments = _fixture.Appointments;
+        var today = new DateTime(2025, 9, 24); 
+        var cabinetNumber = "105"; 
+
+        var appointmentPatientPassports = appointments
+            .Where(a => a.CabinetNumber == cabinetNumber
+                        && a.AppointmentTime.Year == today.Year
+                        && a.AppointmentTime.Month == today.Month)
+            .OrderBy(a => a.AppointmentTime)
+            .Select(a => a.Patient.PassportNumber)
+            .ToArray();
+
+        var expected = new[]
+        {
+            "2222 000005" 
+        };
+
+        Assert.Equal(expected, appointmentPatientPassports);
+    }
+
 }
